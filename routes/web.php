d4c2f173828c01efe4d76556b36b42b3487040c5
jerrysanguyo <jsanguyo1624@gmail.com>
2024-04-22 20:08:57 +0800

    disable the registration

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 Route::get('/unauthorized-access', [UnauthorizedController::class, 'index'])
     ->name('unauthorized');
     
